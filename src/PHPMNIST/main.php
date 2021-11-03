1956b897c13ef16a9bf04f734afe30acd8e0ad16
mudde <olaf.mudde@xs4all.nl>
2021-11-03 15:45:05 +0100

    Randomizer :)

--- src/PHPMNIST/main.php
+++ src/PHPMNIST/main.php
@@ -4,5 +4,10 @@ include '../../vendor/autoload.php';
 
 use SciPhp\NumPhp as np;
 
-$a = np::random()->randn();
-echo('random float: '.$a);
+echo '<pre>';
+try {
+    $a = np::random();
+    var_dump($a->randn(10,1,10));
+} catch (Exception $e) {
+    var_dump($e->getMessage());
+}
